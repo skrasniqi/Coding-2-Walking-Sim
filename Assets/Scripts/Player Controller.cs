8157e2d474edc0d87d8254b2a391feee205285ae
skrasniqi <skrasniq@pratt.edu>
2026-01-29 15:15:32 -0500

    Character Controller has Gravity

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -3,7 +3,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 12;
-    
+    public float gravity = 9.8f;
+    public float groundCheckRadius = 0.15f;
+    public LayerMask groundLayer;
+
+
+
+    private bool isGrounded;
+    private Vector3 velocity;
+    private Transform feet;
 
     private CharacterController controller;
 
@@ -11,10 +19,19 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        feet = transform.Find("feet");
     }
 
 
     private void Update()
+    {
+        CheckIsGrounded();
+        Move();
+        ApplyGravity();
+    }
+
+
+    private void Move()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
@@ -22,4 +39,26 @@ public class PlayerController : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move);
     }
+
+    private void CheckIsGrounded()
+    {
+        isGrounded = Physics.CheckSphere(feet.position, groundCheckRadius, groundLayer);
+    }
+
+
+    private void ApplyGravity()
+    {
+        velocity += Vector3.down * gravity * Time.deltaTime;
+        if(isGrounded)
+        {
+            velocity = Vector3.zero;
+
+            controller.Move(velocity);
+        }
+    }
 }
+
+
+
+
+
